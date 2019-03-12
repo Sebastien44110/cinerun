c57a916f8fb907aebc32042b06fcdaafa63356d2
Yasugui974 <guillaumepavade@hotmail.fr>
2019-03-12 15:42:36 +0400

    Ajout cle application

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Cinerun'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('cinerun974'),
 
     'cipher' => 'AES-256-CBC',
 
